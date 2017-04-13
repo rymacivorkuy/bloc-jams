cb6ebb06a5c87ead5ebe6430e26de1d8f87d1ff6
Ryan Kuykendall <rymacivorkuy@gmail.com>
2017-04-13 15:39:13 -0500

    Completed checkpoint 12

--- scripts/album.js
+++ scripts/album.js
@@ -28,24 +28,45 @@ var albumMarconi = {
   ]
  };
 
-function createSongRow(songNumber, songName, songLength) {
+var albumRyan = {
+  title: 'My First Album',
+  artist: 'Ryan Kuykendall',
+  label: 'Bloc Records',
+  year: '2017',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    { title: 'First Song', duration: '1:01' },
+    { title: 'Second Song', duration: '5:01' },
+    { title: 'Third Song', duration: '3:21'},
+    { title: 'Fourth Song', duration: '3:14' },
+    { title: 'Fifth Song', duration: '2:15'},
+    { title: 'Sixth Song', duration: '3:14' },
+    { title: 'Seventh Song', duration: '2:11' },
+    { title: 'Eighth Song', duration: '1:53' },
+    { title: 'Ninth Song', duration: '6:22' },
+    { title: 'Tenth Song', duration: '8:34' }
+  ]
+ };
+
+var createSongRow = function(songNumber, songName, songLength) {
   var template =
       '<tr class="album-view-song-item">'
-    + ' <td class="song-item-number">' + songNumber + '</td>'
+    + ' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
     + ' <td class="song-item-title">' + songName + '</td>'
     + ' <td class="song-item-duration">' + songLength + '</td>'
     ;
   return template;
 };
 
-//just in case the short-hand doesn't work... var setCurrentAlbum = function(album) {
-function setCurrentAlbum(album) {
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function(album) {
   // #1
-  var albumTitle = document.getElementsByClassName('album-view-title')[0];
-  var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-  var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-  var albumImage = document.getElementsByClassName('album-cover-art')[0];
-  var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 
   // #2
   albumTitle.firstChild.nodeValue = album.title;
@@ -62,7 +83,34 @@ function setCurrentAlbum(album) {
   }
 };
 
-//just in case the short-hand doesn't work... window.onload = function() {
-function window.onload() {
+var songListContainer = document.getElementsByClassName("album-view-song-list")[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
+window.onload = function() {
   setCurrentAlbum(albumPicasso);
+
+  songListContainer.addEventListener("mouseover", function(event) {
+    if(event.target.parentElement.className === 'album-view-song-item') {
+      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+    }
+  });
+
+  for(var i = 0; i < songRows.length; i++) {
+    songRows[i].addEventListener('mouseleave', function(event) {
+      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    });
+  }
+
+  var albums = [albumPicasso, albumMarconi, albumRyan];
+  var index = 1;
+  albumImage.addEventListener("click", function(event) {
+    setCurrentAlbum(albums[index]);
+    index++;
+    if(index == albums.length) {
+      index = 0;
+    }
+  });
 };
