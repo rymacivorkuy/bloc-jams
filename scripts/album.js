89374433dd40b2e3d2cd12b08d47a7f2dcf24b94
Ryan Kuykendall <rymacivorkuy@gmail.com>
2017-04-18 12:40:21 -0500

    Completed Checkpoint 13

--- scripts/album.js
+++ scripts/album.js
@@ -1,61 +1,61 @@
 var albumPicasso = {
-  title: "The Colors",
-  artist: "Pablo Picasso",
-  label: "Cubism",
-  year: "1881",
-  albumArtUrl: "assets/images/album_covers/01.png",
-  songs: [
-    { title: "Blue", duration: "4:26" },
-    { title: "Green", duration: "3:14" },
-    { title: "Red", duration: "5:01" },
-    { title: "Pink", duration: "3:21" },
-    { title: "Magenta", duration: "2:15" }
-  ]
+    title: "The Colors",
+    artist: "Pablo Picasso",
+    label: "Cubism",
+    year: "1881",
+    albumArtUrl: "assets/images/album_covers/01.png",
+    songs: [
+        { title: "Blue", duration: "4:26" },
+        { title: "Green", duration: "3:14" },
+        { title: "Red", duration: "5:01" },
+        { title: "Pink", duration: "3:21" },
+        { title: "Magenta", duration: "2:15" }
+    ]
 };
 
 var albumMarconi = {
-  title: 'The Telephone',
-  artist: 'Guglielmo Marconi',
-  label: 'EM',
-  year: '1909',
-  albumArtUrl: 'assets/images/album_covers/20.png',
-  songs: [
-    { title: 'Hello, Operator?', duration: '1:01' },
-    { title: 'Ring, ring, ring', duration: '5:01' },
-    { title: 'Fits in your pocket', duration: '3:21'},
-    { title: 'Can you hear me now?', duration: '3:14' },
-    { title: 'Wrong phone number', duration: '2:15'}
-  ]
- };
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
 
 var albumRyan = {
-  title: 'My First Album',
-  artist: 'Ryan Kuykendall',
-  label: 'Bloc Records',
-  year: '2017',
-  albumArtUrl: 'assets/images/album_covers/01.png',
-  songs: [
-    { title: 'First Song', duration: '1:01' },
-    { title: 'Second Song', duration: '5:01' },
-    { title: 'Third Song', duration: '3:21'},
-    { title: 'Fourth Song', duration: '3:14' },
-    { title: 'Fifth Song', duration: '2:15'},
-    { title: 'Sixth Song', duration: '3:14' },
-    { title: 'Seventh Song', duration: '2:11' },
-    { title: 'Eighth Song', duration: '1:53' },
-    { title: 'Ninth Song', duration: '6:22' },
-    { title: 'Tenth Song', duration: '8:34' }
-  ]
- };
+    title: 'My First Album',
+    artist: 'Ryan Kuykendall',
+    label: 'Bloc Records',
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'First Song', duration: '1:01' },
+        { title: 'Second Song', duration: '5:01' },
+        { title: 'Third Song', duration: '3:21'},
+        { title: 'Fourth Song', duration: '3:14' },
+        { title: 'Fifth Song', duration: '2:15'},
+        { title: 'Sixth Song', duration: '3:14' },
+        { title: 'Seventh Song', duration: '2:11' },
+        { title: 'Eighth Song', duration: '1:53' },
+        { title: 'Ninth Song', duration: '6:22' },
+        { title: 'Tenth Song', duration: '8:34' }
+    ]
+};
 
 var createSongRow = function(songNumber, songName, songLength) {
-  var template =
-      '<tr class="album-view-song-item">'
+    var template =
+    '<tr class="album-view-song-item">'
     + ' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
     + ' <td class="song-item-title">' + songName + '</td>'
     + ' <td class="song-item-duration">' + songLength + '</td>'
     ;
-  return template;
+    return template;
 };
 
 var albumTitle = document.getElementsByClassName('album-view-title')[0];
@@ -65,52 +65,87 @@ var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
-  // #1
 
+    albumTitle.firstChild.nodeValue = album.title;
+    albumArtist.firstChild.nodeValue = album.artist;
+    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+    albumImage.setAttribute('src', album.albumArtUrl);
 
-  // #2
-  albumTitle.firstChild.nodeValue = album.title;
-  albumArtist.firstChild.nodeValue = album.artist;
-  albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-  albumImage.setAttribute('src', album.albumArtUrl);
+    albumSongList.innerHTML = '';
 
-  // #3
-  albumSongList.innerHTML = '';
+    for (var i = 0; i < album.songs.length; i++) {
+        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    }
+};
 
-  // #4
-  for (var i = 0; i < album.songs.length; i++) {
-    albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-  }
+var clickHandler = function(targetElement) {
+
+    var songItem = getSongItem(targetElement);
+
+    if (currentlyPlayingSong === null) {
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+         songItem.innerHTML = playButtonTemplate;
+         currentlyPlayingSong = null;
+
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+
 };
 
 var songListContainer = document.getElementsByClassName("album-view-song-list")[0];
 var songRows = document.getElementsByClassName('album-view-song-item');
 
-// Album button templates
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+
+var currentlyPlayingSong = null;
+
 
 window.onload = function() {
-  setCurrentAlbum(albumPicasso);
+    setCurrentAlbum(albumPicasso);
 
-  songListContainer.addEventListener("mouseover", function(event) {
-    if(event.target.parentElement.className === 'album-view-song-item') {
-      event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-    }
-  });
+    songListContainer.addEventListener("mouseover", function(event) {
+        if(event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+            var songItem = getSongItem(event.target);
 
-  for(var i = 0; i < songRows.length; i++) {
-    songRows[i].addEventListener('mouseleave', function(event) {
-      this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
+        }
     });
-  }
 
-  var albums = [albumPicasso, albumMarconi, albumRyan];
-  var index = 1;
-  albumImage.addEventListener("click", function(event) {
-    setCurrentAlbum(albums[index]);
-    index++;
-    if(index == albums.length) {
-      index = 0;
+    for(var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+
+            var songItem = getSongItem(event.target);
+            var songItemNumber = songItem.getAttribute('data-song-number');
+
+                if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;
+             }
+        });
+
+        songRows[i].addEventListener('click', function(event) {
+            clickHandler(event.target);
+        });
     }
-  });
+
+    var albums = [albumPicasso, albumMarconi, albumRyan];
+    var index = 1;
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if(index == albums.length) {
+            index = 0;
+        }
+    });
 };
