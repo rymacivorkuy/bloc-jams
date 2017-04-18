42fcaf411af29c342bf27198050a7a2c23e611d4
Ryan Kuykendall <rymacivorkuy@gmail.com>
2017-04-18 15:27:44 -0500

    Added var getSongItem

--- scripts/album.js
+++ scripts/album.js
@@ -78,6 +78,24 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }
+};
+
 var clickHandler = function(targetElement) {
 
     var songItem = getSongItem(targetElement);
@@ -107,7 +125,6 @@ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause">
 
 
 var currentlyPlayingSong = null;
-
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
